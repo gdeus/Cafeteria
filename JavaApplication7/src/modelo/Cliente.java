5c1291209d9b9023076ee1c6d0248399aae79af8
gdeus <dedeus.gabriel@gmail.com>
2015-10-22 20:50:11 -0200

    Adicionado o pacote modelo com a classe abstrata Pessoa com os seguintes  tipos GARCOM, CAIXA, GERENTE, CLIENTE e COZINHEIRO

---
+++ JavaApplication7/src/modelo/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author gabri
+ */
+public class Cliente extends Pessoa{
+
+    public Cliente(String login, String password) {
+        super(login, password);
+    }
+}   
---
+++ JavaApplication7/src/modelo/Garcom.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author gabri
+ */
+public class Garcom extends Pessoa{
+
+    public Garcom(String login, String password) {
+        super(login, password);
+    }
+    
+}
